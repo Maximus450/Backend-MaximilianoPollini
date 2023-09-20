7c87437e5f238f107f7bd70ae86c0890c397aa23
Maximus450 <lordeddardstark14@gmail.com>
2023-09-19 21:48:15 -0300

    Segunda entrega

--- productmanager.js
+++ productmanager.js
@@ -1,60 +1,87 @@
+import { promises as fs } from "fs";
+
 class ProductManager {
   constructor() {
-    this.items = [];
+    this.items = "./products.txt";
+    this.products = [];
   }
 
   static id = 0;
 
-  addProduct(tittle, description, price, thumbnail, code, stock) {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].code === code) {
-        console.log(`The Code ${code} is repeated`);
-        break;
-      }
-    }
-
-    const newProduct = {
-      tittle,
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    ProductManager.id++;
+    let newProduct = {
+      title,
       description,
       price,
       thumbnail,
       code,
       stock,
+      id: ProductManager.id,
     };
-
-    if (!Object.values(newProduct).includes(undefined)) {
-      ProductManager.id++;
-      this.items.push({ ...newProduct, id: ProductManager.id });
-    } else {
-      console.log("There is a missing field");
-    }
+    this.products.push(newProduct);
+    await fs.writeFile(this.items, JSON.stringify(this.products));
   }
 
-  getProduct() {
-    return this.items;
+  async readProducts() {
+    let reply = await fs.readFile(this.items, "utf-8");
+    return JSON.parse(reply);
   }
 
-  check(id) {
-    return this.items.find((product) => product.id === id);
+  async getProducts() {
+    let answer = await this.readProducts();
+    return console.log(answer);
   }
 
-  getProductById(id) {
-    !this.check(id) ? console.log("Not Found") : console.log(this.check(id));
+  async check(id) {
+    let response = await this.readProducts();
+    return response.find((product) => product.id === id);
+  }
+
+  async getProductById(id) {
+    !(await this.check(id))
+      ? console.log("Not Found")
+      : console.log(await this.check(id));
+  }
+
+  async deleteProductById(id) {
+    let response = await this.readProducts();
+    let productFilter = response.filter((products) => products.id != id);
+    await fs.writeFile(this.items, JSON.stringify(productFilter));
+  }
+
+  async updateProducts({ id, ...product }) {
+    await this.deleteProductById(id);
+    let oldProduct = await this.readProducts();
+    let modifiedProduct = [
+      {
+        ...product,
+        id,
+      },
+      ...oldProduct,
+    ];
+    await fs.writeFile(this.items, JSON.stringify(modifiedProduct));
   }
 }
 
 const products = new ProductManager();
 
-console.log(products.getProduct());
+products.addProduct("name001", "description001", 50, "img001", "001", 1);
+products.addProduct("name002", "description002", 50, "img002", "002", 1);
+products.addProduct("name003", "description003", 50, "img002", "003", 1);
 
-products.addProduct("name001", "description001", 50, "img001", "asdfg", 1);
-products.addProduct("name002", "description002", 50, "img002", "asdfgh", 1);
+/* products.getProducts(); */
 
-console.log(products.getProduct());
+/* products.getProductById(1);
+products.getProductById(4); */
 
-products.getProductById(1);
-products.getProductById(3);
+/* products.deleteProductById(3); */
 
-
-products.addProduct("name003", "description003", 50, "img003", "asdfg", 1);
-products.addProduct("name003", "description003", 50, "asdfg", 1);
+/* products.updateProducts({
+  title: "name001",
+  description: "description001",
+  price: 100,
+  thumbnail: "img001",
+  code: "001",
+  id: 1,
+}); */
